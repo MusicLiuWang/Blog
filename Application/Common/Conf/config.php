1c15783136cd859c9af672b1d5f4d107dffcb04c
Git-Mrs liu <416185244@qq.com>
2016-02-26 15:56:37 +0800

    Mr Liu

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,10 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  '115.159.47.194', // 服务器地址
+    'DB_NAME'               =>  'myblog',          // 数据库名
+    'DB_USER'               =>  'wml',      // 用户名
+    'DB_PWD'                =>  'wml',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
 );
